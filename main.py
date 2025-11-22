3b2d860e575dbd17bb72763bde3bb118eaced9f9
mafelfifi <fikiraschalew771@gmail.com>
2025-11-22 13:29:40 -0800

    Comprehensive Amharic Information Storage and Retrieval System.

---
+++ main.py
@@ -0,0 +1,12 @@
+# Main Application
+
+# Importing required libraries
+import amhisr_search
+
+# Main function
+def main():
+    print("Welcome to the Amharic Information Storage and Retrieval System!")
+    amhisr_search.run()
+
+if __name__ == '__main__':
+    main()
